ae8b23978be4dea9a3989de201726532280960d9
damon <zhaorong@outlook.com>
2023-02-24 21:57:36 +0800

    Update Reflect.cs

--- Source/Euonia.Core/Reflection/Reflect.cs
+++ Source/Euonia.Core/Reflection/Reflect.cs
@@ -213,21 +213,219 @@ public static class Reflect
     }
 
     /// <summary>
-    /// Gets the property.
+    /// Checks whether <paramref name="givenType"/> implements/inherits <paramref name="genericType"/>.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <typeparam name="TValue">The type of the t value.</typeparam>
-    /// <param name="property">The property.</param>
-    /// <returns>PropertyInfo.</returns>
-    //private static PropertyInfo GetProperty<T, TValue>(Expression<Func<T, TValue>> property)
-    //{
-    //    if (property.Body.NodeType == ExpressionType.Convert)
-    //    {
-    //        return ((MemberExpression)((UnaryExpression)property.Body).Operand).Member as PropertyInfo;
-    //    }
+    /// <param name="givenType">Type to check</param>
+    /// <param name="genericType">Generic type</param>
+    public static bool IsAssignableToGenericType(Type givenType, Type genericType)
+    {
+        var givenTypeInfo = givenType.GetTypeInfo();
 
-    //    return ((MemberExpression)property.Body).Member as PropertyInfo;
-    //}
+        if (givenTypeInfo.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+        {
+            return true;
+        }
+
+        foreach (var interfaceType in givenTypeInfo.GetInterfaces())
+        {
+            if (interfaceType.GetTypeInfo().IsGenericType && interfaceType.GetGenericTypeDefinition() == genericType)
+            {
+                return true;
+            }
+        }
+
+        return givenTypeInfo.BaseType != null && IsAssignableToGenericType(givenTypeInfo.BaseType, genericType);
+    }
+
+    public static List<Type> GetImplementedGenericTypes(Type givenType, Type genericType)
+    {
+        var result = new List<Type>();
+        AddImplementedGenericTypes(result, givenType, genericType);
+        return result;
+    }
+
+    private static void AddImplementedGenericTypes(ICollection<Type> result, Type givenType, Type genericType)
+    {
+        var givenTypeInfo = givenType.GetTypeInfo();
+
+        if (givenTypeInfo.IsGenericType && givenType.GetGenericTypeDefinition() == genericType)
+        {
+            result.AddIfNotContains(givenType);
+        }
+
+        foreach (var interfaceType in givenTypeInfo.GetInterfaces())
+        {
+            if (interfaceType.GetTypeInfo().IsGenericType && interfaceType.GetGenericTypeDefinition() == genericType)
+            {
+                result.AddIfNotContains(interfaceType);
+            }
+        }
+
+        if (givenTypeInfo.BaseType == null)
+        {
+            return;
+        }
+
+        AddImplementedGenericTypes(result, givenTypeInfo.BaseType, genericType);
+    }
+
+    /// <summary>
+    /// Tries to gets an of attribute defined for a class member and it's declaring type including inherited attributes.
+    /// Returns default value if it's not declared at all.
+    /// </summary>
+    /// <typeparam name="TAttribute">Type of the attribute</typeparam>
+    /// <param name="memberInfo">MemberInfo</param>
+    /// <param name="defaultValue">Default value (null as default)</param>
+    /// <param name="inherit">Inherit attribute from base classes</param>
+    public static TAttribute GetSingleAttributeOrDefault<TAttribute>(MemberInfo memberInfo, TAttribute defaultValue = default, bool inherit = true)
+        where TAttribute : Attribute
+    {
+        //Get attribute on the member
+        if (memberInfo.IsDefined(typeof(TAttribute), inherit))
+        {
+            return memberInfo.GetCustomAttributes(typeof(TAttribute), inherit).Cast<TAttribute>().First();
+        }
+
+        return defaultValue;
+    }
+
+    /// <summary>
+    /// Tries to gets an of attribute defined for a class member and it's declaring type including inherited attributes.
+    /// Returns default value if it's not declared at all.
+    /// </summary>
+    /// <typeparam name="TAttribute">Type of the attribute</typeparam>
+    /// <param name="memberInfo">MemberInfo</param>
+    /// <param name="defaultValue">Default value (null as default)</param>
+    /// <param name="inherit">Inherit attribute from base classes</param>
+    public static TAttribute GetSingleAttributeOfMemberOrDeclaringTypeOrDefault<TAttribute>(MemberInfo memberInfo, TAttribute defaultValue = default, bool inherit = true)
+        where TAttribute : class
+    {
+        return memberInfo.GetCustomAttributes(inherit).OfType<TAttribute>().FirstOrDefault()
+               ?? memberInfo.DeclaringType?.GetTypeInfo().GetCustomAttributes(true).OfType<TAttribute>().FirstOrDefault()
+               ?? defaultValue;
+    }
+
+    /// <summary>
+    /// Tries to gets attributes defined for a class member and it's declaring type including inherited attributes.
+    /// </summary>
+    /// <typeparam name="TAttribute">Type of the attribute</typeparam>
+    /// <param name="memberInfo">MemberInfo</param>
+    /// <param name="inherit">Inherit attribute from base classes</param>
+    public static IEnumerable<TAttribute> GetAttributesOfMemberOrDeclaringType<TAttribute>(MemberInfo memberInfo, bool inherit = true)
+        where TAttribute : class
+    {
+        var customAttributes = memberInfo.GetCustomAttributes(inherit).OfType<TAttribute>();
+        var declaringTypeCustomAttributes =
+            memberInfo.DeclaringType?.GetTypeInfo().GetCustomAttributes(true).OfType<TAttribute>();
+        return declaringTypeCustomAttributes != null
+            ? customAttributes.Concat(declaringTypeCustomAttributes).Distinct()
+            : customAttributes;
+    }
+
+    /// <summary>
+    /// Gets value of a property by it's full path from given object
+    /// </summary>
+    public static object GetValue(object obj, Type objectType, string propertyPath)
+    {
+        var value = obj;
+        var currentType = objectType;
+        var objectPath = currentType.FullName;
+        var absolutePropertyPath = propertyPath;
+        if (objectPath != null && absolutePropertyPath.StartsWith(objectPath))
+        {
+            absolutePropertyPath = absolutePropertyPath.Replace(objectPath + ".", "");
+        }
+
+        foreach (var propertyName in absolutePropertyPath.Split('.'))
+        {
+            var property = currentType.GetProperty(propertyName);
+            if (property != null)
+            {
+                if (value != null)
+                {
+                    value = property.GetValue(value, null);
+                }
+
+                currentType = property.PropertyType;
+            }
+            else
+            {
+                value = null;
+                break;
+            }
+        }
+
+        return value;
+    }
+
+    /// <summary>
+    /// Sets value of a property by it's full path on given object
+    /// </summary>
+    public static void SetValue(object obj, Type objectType, string propertyPath, object value)
+    {
+        var currentType = objectType;
+        PropertyInfo property;
+        var objectPath = currentType.FullName;
+        var absolutePropertyPath = propertyPath;
+        if (absolutePropertyPath.StartsWith(objectPath))
+        {
+            absolutePropertyPath = absolutePropertyPath.Replace(objectPath + ".", "");
+        }
+
+        var properties = absolutePropertyPath.Split('.');
+
+        if (properties.Length == 1)
+        {
+            property = objectType.GetProperty(properties.First());
+            property.SetValue(obj, value);
+            return;
+        }
+
+        for (var i = 0; i < properties.Length - 1; i++)
+        {
+            property = currentType.GetProperty(properties[i]);
+            obj = property.GetValue(obj, null);
+            currentType = property.PropertyType;
+        }
+
+        property = currentType.GetProperty(properties.Last());
+        property.SetValue(obj, value);
+    }
+
+    /// <summary>
+    /// Get all the constant values in the specified type (including the base type).
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public static IEnumerable<string> GetPublicConstantsRecursively(Type type)
+    {
+        const int maxRecursiveParameterValidationDepth = 8;
+
+        var publicConstants = new List<string>();
+
+        static void Recursively(List<string> constants, Type targetType, int currentDepth)
+        {
+            if (currentDepth > maxRecursiveParameterValidationDepth)
+            {
+                return;
+            }
+
+            constants.AddRange(targetType.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                                         .Where(x => x.IsLiteral && !x.IsInitOnly)
+                                         .Select(x => x.GetValue(null).ToString()));
+
+            var nestedTypes = targetType.GetNestedTypes(BindingFlags.Public);
+
+            foreach (var nestedType in nestedTypes)
+            {
+                Recursively(constants, nestedType, currentDepth + 1);
+            }
+        }
+
+        Recursively(publicConstants, type, 1);
+
+        return publicConstants.ToArray();
+    }
 }
 
 /// <summary>
@@ -361,5 +559,30 @@ public static class Reflect<TTarget>
         var propertyInfo = GetProperty(property);
 
         return propertyInfo.GetValue(item);
+    }
+
+    public static bool IsAssignableToGenericType(Type genericType)
+    {
+        return Reflect.IsAssignableToGenericType(typeof(TTarget), genericType);
+    }
+
+    public static List<Type> GetImplementedGenericTypes(Type genericType)
+    {
+        return Reflect.GetImplementedGenericTypes(typeof(TTarget), genericType);
+    }
+
+    public static object GetValue(TTarget obj, string propertyPath)
+    {
+        return Reflect.GetValue(obj, typeof(TTarget), propertyPath);
+    }
+
+    public static void SetValue(TTarget obj, string propertyPath, object value)
+    {
+        Reflect.SetValue(obj, typeof(TTarget), propertyPath, value);
+    }
+
+    public static IEnumerable<string> GetPublicConstantsRecursively()
+    {
+        return Reflect.GetPublicConstantsRecursively(typeof(TTarget));
     }
 }
