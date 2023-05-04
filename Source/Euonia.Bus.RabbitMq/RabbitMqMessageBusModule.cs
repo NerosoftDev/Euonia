d8a48c720ea9951aaf4e2590ceb85242af2f52c8
damon <zhaorong@outlook.com>
2023-05-04 23:43:23 +0800

    Optimize ServiceBus

--- Source/Euonia.Bus.RabbitMq/RabbitMqMessageBusModule.cs
+++ Source/Euonia.Bus.RabbitMq/RabbitMqMessageBusModule.cs
@@ -8,65 +8,13 @@ namespace Nerosoft.Euonia.Bus.RabbitMq;
 /// 
 /// </summary>
 [DependsOn(typeof(MessageBusModule))]
-[DependsOn(typeof(RabbitMqCommandBusModule), typeof(RabbitMqEventBusModule))]
 public class RabbitMqMessageBusModule : ModuleContextBase
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public const string CommandBusProviderName = "rabbitmq";
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public const string EventBusProviderName = "rabbitmq";
-
     /// <inheritdoc />
     public override void ConfigureServices(ServiceConfigurationContext context)
     {
-        context.Services.TryAddSingleton<MessageConversionDelegate>(_ =>
-        {
-            return MessageConverter.Convert;
-        });
-        //context.Items.TryGetValue("COMMAND_BUS_PROVIDER", value =>
-        //{
-        //    if (value is CommandBusProviderName or null)
-        //    {
-        //        context.Services.AddRabbitCommandBus();
-        //    }
-        //});
-        //context.Items.TryGetValue("EVENT_BUS_PROVIDER", value =>
-        //{
-        //    if (value is EventBusProviderName or null)
-        //    {
-        //        context.Services.AddRabbitCommandBus();
-        //    }
-        //});
-    }
-}
-
-/// <summary>
-/// 
-/// </summary>
-[DependsOn(typeof(MessageBusModule))]
-public class RabbitMqCommandBusModule : ModuleContextBase
-{
-    /// <inheritdoc />
-    public override void ConfigureServices(ServiceConfigurationContext context)
-    {
+        context.Services.TryAddSingleton<MessageConversionDelegate>(_ => MessageConverter.Convert);
         context.Services.AddRabbitMqCommandBus();
-    }
-}
-
-/// <summary>
-/// 
-/// </summary>
-[DependsOn(typeof(MessageBusModule))]
-public class RabbitMqEventBusModule : ModuleContextBase
-{
-    /// <inheritdoc />
-    public override void ConfigureServices(ServiceConfigurationContext context)
-    {
         context.Services.AddRabbitMqEventBus();
     }
 }
