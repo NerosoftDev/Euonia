694704843431920eed89b84061c5c9c4240ff057
damon <zhaorong@outlook.com>
2023-07-15 18:35:44 +0800

    Add protos.

---
+++ Source/Euonia.Grpc/Core/GrpcResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Google.Protobuf;
+
+/// <summary>
+/// To be added.
+/// </summary>
+public partial class GrpcResponse
+{
+    /// <summary>
+    /// To be added.
+    /// </summary>
+    /// <param name="value"></param>
+    public GrpcResponse(object value)
+    {
+        Data = value.GetType().IsClass ? JsonSerializer.Serialize(value) : value.ToString();
+    }
+}
