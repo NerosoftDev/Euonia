28311c1337fb683323589e3221a92a8da9732984
damon <zhaorong@outlook.com>
2023-11-28 11:29:40 +0800

    Remove unused constructor.

--- Source/Euonia.Bus.Abstract/Events/MessageSubscribedEventArgs.cs
+++ Source/Euonia.Bus.Abstract/Events/MessageSubscribedEventArgs.cs
@@ -1,48 +1,37 @@
 ﻿namespace Nerosoft.Euonia.Bus;
 
 /// <summary>
-/// Class MessageSubscribedEventArgs.
-/// Implements the <see cref="EventArgs" />
+/// Represents the message was subscribed.
 /// </summary>
-/// <seealso cref="EventArgs" />
 public class MessageSubscribedEventArgs : EventArgs
 {
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MessageSubscribedEventArgs"/> class.
-    /// </summary>
-    /// <param name="messageType">Type of the message.</param>
-    /// <param name="handlerType">Type of the handler.</param>
-    public MessageSubscribedEventArgs(Type messageType, Type handlerType)
-        : this(messageType.FullName, handlerType)
-    {
-        MessageType = messageType;
-    }
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MessageSubscribedEventArgs"/> class.
+	/// </summary>
+	/// <param name="channel"></param>
+	/// <param name="messageType"></param>
+	/// <param name="handlerType"></param>
+	public MessageSubscribedEventArgs(string channel, Type messageType, Type handlerType)
+	{
+		Channel = channel;
+		MessageType = messageType;
+		HandlerType = handlerType;
+	}
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MessageSubscribedEventArgs"/> class.
-    /// </summary>
-    /// <param name="messageName"></param>
-    /// <param name="handlerType"></param>
-    public MessageSubscribedEventArgs(string messageName, Type handlerType)
-    {
-        MessageName = messageName;
-        HandlerType = handlerType;
-    }
+	/// <summary>
+	/// Gets the message name.
+	/// </summary>
+	public string Channel { get; }
 
-    /// <summary>
-    /// Gets the type of the message.
-    /// </summary>
-    /// <value>The type of the message.</value>
-    public Type MessageType { get; }
+	/// <summary>
+	/// Gets the type of the message.
+	/// </summary>
+	/// <value>The type of the message.</value>
+	public Type MessageType { get; }
 
-    /// <summary>
-    /// Gets the message name.
-    /// </summary>
-    public string MessageName { get; }
-
-    /// <summary>
-    /// Gets the type of the handler.
-    /// </summary>
-    /// <value>The type of the handler.</value>
-    public Type HandlerType { get; }
+	/// <summary>
+	/// Gets the type of the handler.
+	/// </summary>
+	/// <value>The type of the handler.</value>
+	public Type HandlerType { get; }
 }
