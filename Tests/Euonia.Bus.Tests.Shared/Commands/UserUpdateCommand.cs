2630801a591c19cef8437cbaf3fbe49801f54887
damon <zhaorong@outlook.com>
2023-11-29 20:34:25 +0800

    Optimize code.

--- Tests/Euonia.Bus.Tests.Shared/Commands/UserUpdateCommand.cs
+++ Tests/Euonia.Bus.Tests.Shared/Commands/UserUpdateCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Nerosoft.Euonia.Bus.Tests.Commands;
+﻿namespace Nerosoft.Euonia.Bus.Tests.Commands;
 
 [Queue]
 public class UserUpdateCommand
