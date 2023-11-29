5ada141cb6eb44b50327a352d181efca09808578
damon <zhaorong@outlook.com>
2023-11-29 22:00:08 +0800

    Refactor unit test.

--- Tests/Euonia.Bus.Tests.Shared/ServiceBusTests.cs
+++ Tests/Euonia.Bus.Tests.Shared/ServiceBusTests.cs
@@ -1,60 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
-using Nerosoft.Euonia.Bus.Tests.Commands;
+﻿namespace Nerosoft.Euonia.Bus.Tests;
 
-namespace Nerosoft.Euonia.Bus.Tests;
-
-public class ServiceBusTests
+public partial class ServiceBusTests
 {
-	private readonly IBus _bus;
-	private readonly bool _dontRunTests;
-
-	public ServiceBusTests(IBus bus, IConfiguration configuration)
-	{
-		_bus = bus;
-		_dontRunTests = configuration.GetValue<bool>("DontRunTests");
-	}
+	[Fact]
+	public partial Task TestSendCommand_HasReponse();
 
 	[Fact]
-	public async Task TestSendCommand_HasReponse()
-	{
-		if (_dontRunTests)
-		{
-			return;
-		}
-		var result = await _bus.SendAsync<UserCreateCommand, int>(new UserCreateCommand());
-		Assert.Equal(1, result);
-	}
+	public partial Task TestSendCommand_NoReponse();
 
 	[Fact]
-	public async Task TestSendCommand_NoReponse()
-	{
-		if (_dontRunTests)
-		{
-			return;
-		}
-		await _bus.SendAsync(new UserCreateCommand());
-		Assert.True(true);
-	}
+	public partial Task TestSendCommand_HasReponse_UseSubscribeAttribute();
 
 	[Fact]
-	public async Task TestSendCommand_HasReponse_UseSubscribeAttribute()
-	{
-		if (_dontRunTests)
-		{
-			return;
-		}
-		var result = await _bus.SendAsync<FooCreateCommand, int>(new FooCreateCommand(), new SendOptions { Channel = "foo.create" });
-		Assert.Equal(1, result);
-	}
-
-	[Fact]
-	public async Task TestSendCommand_HasReponse_MessageHasResultInherites()
-	{
-		if (_dontRunTests)
-		{
-			return;
-		}
-		var result = await _bus.SendAsync<int>(new FooCreateCommand(), new SendOptions { Channel = "foo.create" });
-		Assert.Equal(1, result);
-	}
+	public partial Task TestSendCommand_HasReponse_MessageHasResultInherites();
 }
