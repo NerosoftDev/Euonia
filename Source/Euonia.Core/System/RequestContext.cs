37fbadbdf935dc20ff18152126348539122dfd74
damon <zhaorong@outlook.com>
2023-12-07 08:40:50 +0800

    Move RequestContext accessor to Modularity project.

---
+++ Source/Euonia.Core/System/RequestContext.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Security.Claims;
+
+namespace System;
+
+/// <summary>
+/// Contains information about the current request.
+/// </summary>
+public sealed class RequestContext
+{
+	/// <summary>
+	/// Gets or sets a unique identifier to represent the connection.
+	/// </summary>
+	public string ConnectionId { get; set; }
+
+	/// <summary>
+	/// Gets or sets the IP address of the remote target. Can be null.
+	/// </summary>
+	public IPAddress RemoteIpAddress { get; set; }
+
+	/// <summary>
+	/// Gets or sets the port of the remote target.
+	/// </summary>
+	public int RemotePort { get; }
+
+	/// <summary>
+	/// Gets a value indicating whether the request is a WebSocket establishment request.
+	/// </summary>
+	public bool IsWebSocketRequest { get; }
+
+	/// <summary>
+	/// Gets or sets the user for this request.
+	/// </summary>
+	public ClaimsPrincipal User { get; set; }
+
+	/// <summary>
+	/// Gets the request headers.
+	/// </summary>
+	/// <returns>The request headers.</returns>
+	public IDictionary<string, string> RequestHeaders { get; set; }
+
+	/// <summary>
+	/// Gets the Authorization HTTP header.
+	/// </summary>
+	public string Authorization => RequestHeaders?.TryGetValue(nameof(Authorization)) ?? default;
+
+	/// <summary>
+	/// Gets or sets the Request-Id HTTP header.
+	/// </summary>
+	public string RequestId => RequestHeaders?.TryGetValue("Request-Id") ?? default;
+
+	/// <summary>
+	/// Gets or sets the <see cref="IServiceProvider"/> that provides access to the request's service container.
+	/// </summary>
+	public IServiceProvider RequestServices { get; set; }
+
+	/// <summary>
+	/// Notifies when the connection underlying this request is aborted and thus request operations should be
+	/// cancelled.
+	/// </summary>
+	public CancellationToken RequestAborted { get; set; }
+
+	/// <summary>
+	/// Gets or sets a unique identifier to represent this request in trace logs.
+	/// </summary>
+	public string TraceIdentifier { get; set; }
+}
--- Source/Euonia.Hosting/HostingModule.cs
+++ Source/Euonia.Hosting/HostingModule.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Nerosoft.Euonia.Modularity;
 
 namespace Nerosoft.Euonia.Hosting;
@@ -9,35 +11,52 @@ namespace Nerosoft.Euonia.Hosting;
 /// </summary>
 public class HostingModule : ModuleContextBase
 {
-    /// <inheritdoc />
-    public override void ConfigureServices(ServiceConfigurationContext context)
-    {
-        context.Services.AddSingleton<IServiceAccessor, ServiceAccessor>();
-        context.Services.AddSerilog();
-        context.Services.AddScopeTransformation();
-        context.Services.AddUserPrincipal();
-        context.Services.AddObjectAccessor<IApplicationBuilder>();
-        context.Services.AddTransient<ExceptionHandlingMiddleware>();
-    }
+	/// <inheritdoc />
+	public override void ConfigureServices(ServiceConfigurationContext context)
+	{
+		context.Services.TryAddScoped<RequestContextAccessor>(provider => GetRequestContext);
+		context.Services.AddSingleton<IServiceAccessor, ServiceAccessor>();
+		context.Services.TryAddScoped<IRequestContextAccessor, DelegateRequestContextAccessor>();
+		context.Services.AddSerilog();
+		context.Services.AddScopeTransformation();
+		context.Services.AddUserPrincipal();
+		context.Services.AddObjectAccessor<IApplicationBuilder>();
+		context.Services.AddTransient<ExceptionHandlingMiddleware>();
 
-    /// <inheritdoc />
-    public override void OnApplicationInitialization(ApplicationInitializationContext context)
-    {
-        base.OnApplicationInitialization(context);
-        var app = context.GetApplicationBuilder();
-        app.UseMiddleware<RequestTraceMiddleware>();
-        app.UseMiddleware<ExceptionHandlingMiddleware>();
-        
-        // Setup the ServiceProvider for IServiceAccessor.
-        app.Use(async (httpContext, next) =>
-        {
-            var accessor = httpContext.RequestServices.GetService<IServiceAccessor>();
-            if (accessor != null)
-            {
-                accessor.ServiceProvider = httpContext.RequestServices;
-            }
+		static RequestContext GetRequestContext(IServiceProvider provider)
+		{
+			var context = provider.GetService<IHttpContextAccessor>()?.HttpContext;
+			if (context == null)
+			{
+				return null;
+			}
+			return new RequestContext
+			{
+				RequestHeaders = (IDictionary<string, string>)(context?.Request?.Headers),
+				ConnectionId = context?.Connection?.Id,
 
-            await next();
-        });
-    }
+			};
+		}
+	}
+
+	/// <inheritdoc />
+	public override void OnApplicationInitialization(ApplicationInitializationContext context)
+	{
+		base.OnApplicationInitialization(context);
+		var app = context.GetApplicationBuilder();
+		app.UseMiddleware<RequestTraceMiddleware>();
+		app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+		// Setup the ServiceProvider for IServiceAccessor.
+		app.Use(async (httpContext, next) =>
+		{
+			var accessor = httpContext.RequestServices.GetService<IServiceAccessor>();
+			if (accessor != null)
+			{
+				accessor.ServiceProvider = httpContext.RequestServices;
+			}
+
+			await next();
+		});
+	}
 }
