3e3b653fd94fa214709ac2be8be26be5763f24f5
damon <zhaorong@outlook.com>
2023-12-09 19:16:24 +0800

    Add message/command/event id.

--- Source/Euonia.Domain/Events/Event.cs
+++ Source/Euonia.Domain/Events/Event.cs
@@ -1,10 +1,14 @@
-﻿namespace Nerosoft.Euonia.Domain;
+﻿using Nerosoft.Euonia.Reflection;
+
+namespace Nerosoft.Euonia.Domain;
 
 /// <summary>
 /// The abstract class implements <see cref="IEvent"/>.
 /// </summary>
 public abstract class Event : IEvent
 {
+	private const string PROPERTY_ID = "nerosoft.euonia.internal.event.id";
+
 	/// <summary>
 	/// Initializes a new instance of the <see cref="Event"/> class.
 	/// </summary>
@@ -12,6 +16,42 @@ public abstract class Event : IEvent
 	{
 		var type = GetType();
 		EventIntent = type.Name;
+		Properties[PROPERTY_ID] = Guid.NewGuid().ToString();
+	}
+
+	/// <summary>
+	/// Gets the extended properties of command.
+	/// </summary>
+	public IDictionary<string, string> Properties { get; set; } = new Dictionary<string, string>();
+
+	/// <summary>
+	/// Gets or sets the command property with specified name.
+	/// </summary>
+	/// <param name="name"></param>
+	public string this[string name]
+	{
+		get => Properties.TryGetValue(name, out var value) ? value : default;
+		set => Properties[name] = value;
+	}
+
+	/// <summary>
+	/// Gets value of property <paramref name="name"/>.
+	/// </summary>
+	/// <param name="name"></param>
+	/// <typeparam name="T"></typeparam>
+	/// <returns></returns>
+	public virtual T GetProperty<T>(string name)
+	{
+		return TypeHelper.CoerceValue<T, string>(this[name]);
+	}
+
+	/// <summary>
+	/// Gets or sets the event identifier.
+	/// </summary>
+	public string EventId
+	{
+		get => this[PROPERTY_ID];
+		set => this[PROPERTY_ID] = value;
 	}
 
 	/// <summary>
