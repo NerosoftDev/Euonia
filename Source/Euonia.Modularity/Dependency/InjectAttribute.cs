9425f1bdf940d5bd72ad7a30ef06e134b3ac0b4d
damon <zhaorong@outlook.com>
2024-09-21 22:41:18 +0800

    Use 'Key' instead of 'Name' in InjectAttribute.

--- Source/Euonia.Modularity/Dependency/InjectAttribute.cs
+++ Source/Euonia.Modularity/Dependency/InjectAttribute.cs
@@ -4,7 +4,7 @@
 namespace System;
 
 /// <summary>
-/// Indicate that the property or parameter would resolved from service container.
+/// Indicate that the property or parameter would resolve from service container.
 /// </summary>
 [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property)]
 public class InjectAttribute : Attribute
@@ -12,14 +12,14 @@ public class InjectAttribute : Attribute
     /// <summary>
     /// Initialize a new instance of <see cref="InjectAttribute"/>.
     /// </summary>
-    /// <param name="name"></param>
-    public InjectAttribute(string name = null)
+    /// <param name="key">An object that specifies the key of service object to get.</param>
+    public InjectAttribute(object key = null)
     {
-        Name = name;
+        Key = key;
     }
 
     /// <summary>
-    /// Specified the service name or service type name.
+    /// An object that specifies the key of service object to get.
     /// </summary>
-    public string Name { get; }
+    public object Key { get; }
 }
