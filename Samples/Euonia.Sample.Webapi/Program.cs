ab37c496bca59694d0d9fdb9128167431c6c0716
damon <zhaorong@outlook.com>
2025-12-11 10:38:16 +0800

    Add Serilog integration for enhanced logging capabilities

--- Samples/Euonia.Sample.Webapi/Program.cs
+++ Samples/Euonia.Sample.Webapi/Program.cs
@@ -1,56 +1,79 @@
 ﻿using Nerosoft.Euonia.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace Nerosoft.Euonia.Sample;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        static void HostBuilderOptionsAction(HostBuilderOptions options)
-        {
-            options.EnableHttp2 = true;
-            //options.ConfigureWebHostBuilder = WebHostBuilderAction;
-        }
+	public static void Main(string[] args)
+	{
+		static void HostBuilderOptionsAction(HostBuilderOptions options)
+		{
+			options.EnableHttp2 = true;
+			//options.ConfigureWebHostBuilder = WebHostBuilderAction;
+			options.ConfigureHostBuilder = builder =>
+			{
+				builder.UseSerilog((context, configuration) =>
+				{
+					configuration.ReadFrom.Configuration(context.Configuration)
+					             .Enrich.FromLogContext()
+					             .Enrich.WithProperty("ApplicationName", context.HostingEnvironment.ApplicationName)
+					             .Enrich.WithProperty("Environment", context.HostingEnvironment);
 
-        //static void WebHostBuilderAction(IWebHostBuilder builder)
-        //{
-        //    builder.ConfigureKestrel(kes =>
-        //    {
-        //        kes.ListenLocalhost(4567, cfg => cfg.Protocols = HttpProtocols.Http2);
-        //        kes.ListenLocalhost(5678, cfg => cfg.Protocols = HttpProtocols.Http1);
-        //    });
-        //}
+					configuration.WriteTo.Logger(config => config.Filter.ByIncludingOnly(@event => @event.Level == LogEventLevel.Debug)
+					                                             .WriteTo.File("Logs/Debug/logs.log", rollingInterval: RollingInterval.Day))
+					             .WriteTo.Logger(config => config.Filter.ByIncludingOnly(@event => @event.Level == LogEventLevel.Warning)
+					                                             .WriteTo.File("Logs/Warning/logs.log", rollingInterval: RollingInterval.Day))
+					             .WriteTo.Logger(config => config.Filter.ByIncludingOnly(@event => @event.Level == LogEventLevel.Error)
+					                                             .WriteTo.File("Logs/Error/logs.log", rollingInterval: RollingInterval.Day))
+					             .WriteTo.Logger(config => config.Filter.ByIncludingOnly(@event => @event.Level == LogEventLevel.Information)
+					                                             .WriteTo.File("Logs/Info/logs.log", rollingInterval: RollingInterval.Day))
+					             .WriteTo.Logger(config => config.Filter.ByIncludingOnly(@event => @event.Level == LogEventLevel.Fatal)
+					                                             .WriteTo.File("Logs/Fatal/logs.log", rollingInterval: RollingInterval.Day));
+				});
+			};
+		}
 
-        HostUtility.Run<Startup>(args, HostBuilderOptionsAction);
-    }
+		// static void WebHostBuilderAction(IWebHostBuilder builder)
+		// {
+		// 	builder.ConfigureKestrel(kes =>
+		// 	{
+		// 	    kes.ListenLocalhost(4567, cfg => cfg.Protocols = HttpProtocols.Http2);
+		// 	    kes.ListenLocalhost(5678, cfg => cfg.Protocols = HttpProtocols.Http1);
+		// 	});
+		// }
 
-    //public static void Main(string[] args)
-    //{
-    //    var builder = WebApplication.CreateBuilder(args);
+		HostUtility.Run<Startup>(args, HostBuilderOptionsAction);
+	}
 
-    //    // Add services to the container.
+	//public static void Main(string[] args)
+	//{
+	//    var builder = WebApplication.CreateBuilder(args);
 
-    //    builder.Services.AddControllers();
-    //    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    //    builder.Services.AddEndpointsApiExplorer();
-    //    builder.Services.AddSwaggerGen();
+	//    // Add services to the container.
 
-    //    var app = builder.Build();
+	//    builder.Services.AddControllers();
+	//    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+	//    builder.Services.AddEndpointsApiExplorer();
+	//    builder.Services.AddSwaggerGen();
 
-    //    // Configure the HTTP request pipeline.
-    //    if (app.Environment.IsDevelopment())
-    //    {
-    //        app.UseSwagger();
-    //        app.UseSwaggerUI();
-    //    }
+	//    var app = builder.Build();
 
-    //    app.UseHttpsRedirection();
+	//    // Configure the HTTP request pipeline.
+	//    if (app.Environment.IsDevelopment())
+	//    {
+	//        app.UseSwagger();
+	//        app.UseSwaggerUI();
+	//    }
 
-    //    app.UseAuthorization();
+	//    app.UseHttpsRedirection();
+
+	//    app.UseAuthorization();
 
 
-    //    app.MapControllers();
+	//    app.MapControllers();
 
-    //    app.Run();
-    //}
+	//    app.Run();
+	//}
 }
