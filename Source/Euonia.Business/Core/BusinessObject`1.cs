ddf2fd8c2cb4f54b6e496064af97275e17dd2c11
damon <zhaorong@outlook.com>
2026-01-06 21:52:48 +0800

    Enhance property registration methods with default value support and improve documentation clarity

--- Source/Euonia.Business/Core/BusinessObject`1.cs
+++ Source/Euonia.Business/Core/BusinessObject`1.cs
@@ -32,20 +32,7 @@ public abstract class BusinessObject<T> : BusinessObject
 	{
 		return RegisterProperty(new PropertyInfo<TValue>(propertyName, friendlyName, typeof(T)));
 	}
-
-	/// <summary>
-	/// Registers a property.
-	/// </summary>
-	/// <typeparam name="TValue">The property value.</typeparam>
-	/// <param name="expression"></param>
-	/// <param name="friendlyName"></param>
-	/// <returns></returns>
-	protected static PropertyInfo<TValue> RegisterProperty<TValue>(Expression<Func<T, object>> expression, string friendlyName = null)
-	{
-		var property = Reflect<T>.GetProperty(expression);
-		return RegisterProperty<TValue>(property.Name, friendlyName);
-	}
-
+	
 	/// <summary>
 	/// Registers a property.
 	/// </summary>
@@ -63,6 +50,33 @@ public abstract class BusinessObject<T> : BusinessObject
 	/// <summary>
 	/// Registers a property.
 	/// </summary>
+	/// <param name="propertyName"></param>
+	/// <param name="friendlyName"></param>
+	/// <param name="defaultValue"></param>
+	/// <typeparam name="TValue"></typeparam>
+	/// <returns></returns>
+	protected static PropertyInfo<TValue> RegisterProperty<TValue>(string propertyName, string friendlyName, Func<TValue> defaultValue)
+	{
+		var property = new PropertyInfo<TValue>(propertyName, friendlyName, typeof(T), defaultValue());
+		return RegisterProperty(property);
+	}
+
+	/// <summary>
+	/// Registers a property.
+	/// </summary>
+	/// <typeparam name="TValue">The property value.</typeparam>
+	/// <param name="expression"></param>
+	/// <param name="friendlyName"></param>
+	/// <returns></returns>
+	protected static PropertyInfo<TValue> RegisterProperty<TValue>(Expression<Func<T, object>> expression, string friendlyName = null)
+	{
+		var property = Reflect<T>.GetProperty(expression);
+		return RegisterProperty<TValue>(property.Name, friendlyName);
+	}
+	
+	/// <summary>
+	/// Registers a property.
+	/// </summary>
 	/// <typeparam name="TValue">The property value.</typeparam>
 	/// <param name="expression"></param>
 	/// <param name="friendlyName"></param>
@@ -72,5 +86,19 @@ public abstract class BusinessObject<T> : BusinessObject
 	{
 		var property = Reflect<T>.GetProperty(expression);
 		return RegisterProperty(property.Name, friendlyName, defaultValue);
+	}
+
+	/// <summary>
+	/// Registers a property.
+	/// </summary>
+	/// <param name="expression"></param>
+	/// <param name="friendlyName"></param>
+	/// <param name="defaultValue"></param>
+	/// <typeparam name="TValue"></typeparam>
+	/// <returns></returns>
+	protected static PropertyInfo<TValue> RegisterProperty<TValue>(Expression<Func<T, object>> expression, string friendlyName, Func<TValue> defaultValue)
+	{
+		var property = Reflect<T>.GetProperty(expression);
+		return RegisterProperty(property.Name, friendlyName, defaultValue());
 	}
 }
