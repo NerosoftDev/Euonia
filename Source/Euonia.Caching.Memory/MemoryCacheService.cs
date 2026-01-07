8ed2c0692d7e07bd23c3d02ca893b7a54751fcfd
damon <zhaorong@outlook.com>
2026-01-07 09:57:37 +0800

    Add async TryGetAsync to ICacheService and improve docs

--- Source/Euonia.Caching.Memory/MemoryCacheService.cs
+++ Source/Euonia.Caching.Memory/MemoryCacheService.cs
@@ -7,163 +7,174 @@ namespace Nerosoft.Euonia.Caching.Memory;
 /// </summary>
 public class MemoryCacheService : ICacheService
 {
-    private readonly MemoryCacheManager _manager;
-    private readonly string _prefix;
+	private readonly MemoryCacheManager _manager;
+	private readonly string _prefix;
 
-    /// <summary>
-    /// Initializes a new instance of the <see cref="MemoryCacheService"/> class.
-    /// </summary>
-    /// <param name="options"></param>
-    public MemoryCacheService(IOptions<MemoryCacheOptions> options)
-    {
-        _manager = new MemoryCacheManager(options.Value);
-        _prefix = options.Value.KeyPrefix;
-    }
+	/// <summary>
+	/// Initializes a new instance of the <see cref="MemoryCacheService"/> class.
+	/// </summary>
+	/// <param name="options"></param>
+	public MemoryCacheService(IOptions<MemoryCacheOptions> options)
+	{
+		_manager = new MemoryCacheManager(options.Value);
+		_prefix = options.Value.KeyPrefix;
+	}
 
-    /// <inheritdoc />
-    public TValue Get<TValue>(string key)
-    {
-        key = RewriteKey(key);
-        return _manager.Instance<TValue>().Get(key);
-    }
+	/// <inheritdoc />
+	public TValue Get<TValue>(string key)
+	{
+		key = RewriteKey(key);
+		return _manager.Instance<TValue>().Get(key);
+	}
 
-    /// <inheritdoc />
-    public bool TryGet<TValue>(string key, out TValue value)
-    {
-        key = RewriteKey(key);
-        var result = _manager.Instance<TValue>().TryGetOrAdd(key, _ => null, out var cache);
-        value = result ? cache.Value : default;
-        return result;
-    }
+	/// <inheritdoc />
+	public bool TryGet<TValue>(string key, out TValue value)
+	{
+		key = RewriteKey(key);
+		var result = _manager.Instance<TValue>().TryGetOrAdd(key, _ => null, out var cache);
+		value = result ? cache.Value : default;
+		return result;
+	}
 
-    /// <inheritdoc />
-    public TValue GetOrAdd<TValue>(string key, Func<TValue> factory, TimeSpan? timeout = null)
-    {
-        key = RewriteKey(key);
-        var result = _manager.Instance<TValue>().GetOrAdd(key, _ =>
-        {
-            var value = factory();
-            return GetCacheItem(key, value, timeout);
-        });
-        return result.Value;
-    }
+	/// <inheritdoc />
+	public TValue GetOrAdd<TValue>(string key, Func<TValue> factory, TimeSpan? timeout = null)
+	{
+		key = RewriteKey(key);
+		var result = _manager.Instance<TValue>().GetOrAdd(key, _ =>
+		{
+			var value = factory();
+			return GetCacheItem(key, value, timeout);
+		});
+		return result.Value;
+	}
 
-    /// <inheritdoc />
-    public TValue GetOrAdd<TValue>(string key, Func<TValue> factory, DateTime timeout, bool isUtcTime = true)
-    {
-        var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
+	/// <inheritdoc />
+	public TValue GetOrAdd<TValue>(string key, Func<TValue> factory, DateTime timeout, bool isUtcTime = true)
+	{
+		var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
 
-        return GetOrAdd(key, factory, timespan);
-    }
+		return GetOrAdd(key, factory, timespan);
+	}
 
-    /// <inheritdoc />
-    public TValue AddOrUpdate<TValue>(string key, Func<TValue> factory, TimeSpan? timeout = null)
-    {
-        var value = factory();
-        return AddOrUpdate(key, value, timeout);
-    }
+	/// <inheritdoc />
+	public TValue AddOrUpdate<TValue>(string key, Func<TValue> factory, TimeSpan? timeout = null)
+	{
+		var value = factory();
+		return AddOrUpdate(key, value, timeout);
+	}
 
-    /// <inheritdoc />
-    public TValue AddOrUpdate<TValue>(string key, Func<TValue> factory, DateTime timeout, bool isUtcTime = true)
-    {
-        var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
-        return AddOrUpdate(key, factory, timespan);
-    }
+	/// <inheritdoc />
+	public TValue AddOrUpdate<TValue>(string key, Func<TValue> factory, DateTime timeout, bool isUtcTime = true)
+	{
+		var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
+		return AddOrUpdate(key, factory, timespan);
+	}
 
-    /// <inheritdoc />
-    public TValue AddOrUpdate<TValue>(string key, TValue value, TimeSpan? timeout = null)
-    {
-        key = RewriteKey(key);
-        var cacheItem = GetCacheItem(key, value, timeout);
-        return _manager.Instance<TValue>().AddOrUpdate(cacheItem, _ => value);
-    }
+	/// <inheritdoc />
+	public TValue AddOrUpdate<TValue>(string key, TValue value, TimeSpan? timeout = null)
+	{
+		key = RewriteKey(key);
+		var cacheItem = GetCacheItem(key, value, timeout);
+		return _manager.Instance<TValue>().AddOrUpdate(cacheItem, _ => value);
+	}
 
-    /// <inheritdoc />
-    public TValue AddOrUpdate<TValue>(string key, TValue value, DateTime timeout, bool isUtcTime = true)
-    {
-        var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
+	/// <inheritdoc />
+	public TValue AddOrUpdate<TValue>(string key, TValue value, DateTime timeout, bool isUtcTime = true)
+	{
+		var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
 
-        return AddOrUpdate(key, value, timespan);
-    }
+		return AddOrUpdate(key, value, timespan);
+	}
 
-    /// <inheritdoc />
-    public TValue AddOrUpdate<TValue>(CacheItem<TValue> item)
-    {
-        RewriteKey(item.Key);
+	/// <inheritdoc />
+	public TValue AddOrUpdate<TValue>(CacheItem<TValue> item)
+	{
+		RewriteKey(item.Key);
 
-        return _manager.Instance<TValue>().AddOrUpdate(item, _ => item.Value);
-    }
+		return _manager.Instance<TValue>().AddOrUpdate(item, _ => item.Value);
+	}
 
-    /// <inheritdoc />
-    public bool Remove<TValue>(string key)
-    {
-        key = RewriteKey(key);
-        return _manager.Instance<TValue>().Remove(key);
-    }
+	/// <inheritdoc />
+	public bool Remove<TValue>(string key)
+	{
+		key = RewriteKey(key);
+		return _manager.Instance<TValue>().Remove(key);
+	}
 
-    /// <inheritdoc />
-    public async Task<TValue> GetOrAddAsync<TValue>(string key, Func<Task<TValue>> factory, TimeSpan? timeout = null, CancellationToken cancellationToken = default)
-    {
-        key = RewriteKey(key);
-        if (TryGet<TValue>(key, out var value))
-        {
-            return value;
-        }
-        value = await factory();
-        var result = _manager.Instance<TValue>().GetOrAdd(key, _ => GetCacheItem(key, value, timeout));
-        return result.Value;
-    }
+	public async Task<Tuple<bool, TValue>> TryGetAsync<TValue>(string key, CancellationToken cancellationToken = default)
+	{
+		return await Task.Run(() =>
+		{
+			key = RewriteKey(key);
+			var result = _manager.Instance<TValue>().TryGetOrAdd(key, _ => null, out var cache);
+			var value = result ? cache.Value : default;
+			return Tuple.Create(result, value);
+		}, cancellationToken);
+	}
 
-    /// <inheritdoc />
-    public async Task<TValue> GetOrAddAsync<TValue>(string key, Func<Task<TValue>> factory, DateTime timeout, bool isUtcTime = true, CancellationToken cancellationToken = default)
-    {
-        var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
-        return await GetOrAddAsync(key, factory, timespan, cancellationToken);
-    }
+	/// <inheritdoc />
+	public async Task<TValue> GetOrAddAsync<TValue>(string key, Func<Task<TValue>> factory, TimeSpan? timeout = null, CancellationToken cancellationToken = default)
+	{
+		key = RewriteKey(key);
+		if (TryGet<TValue>(key, out var value))
+		{
+			return value;
+		}
+		value = await factory();
+		var result = _manager.Instance<TValue>().GetOrAdd(key, _ => GetCacheItem(key, value, timeout));
+		return result.Value;
+	}
 
-    /// <inheritdoc />
-    public async Task<TValue> AddOrUpdateAsync<TValue>(string key, Func<Task<TValue>> factory, TimeSpan? timeout = null, CancellationToken cancellationToken = default)
-    {
-        key = RewriteKey(key);
-        var value = await factory();
-        return AddOrUpdate(key, value, timeout);
-    }
+	/// <inheritdoc />
+	public async Task<TValue> GetOrAddAsync<TValue>(string key, Func<Task<TValue>> factory, DateTime timeout, bool isUtcTime = true, CancellationToken cancellationToken = default)
+	{
+		var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
+		return await GetOrAddAsync(key, factory, timespan, cancellationToken);
+	}
 
-    /// <inheritdoc />
-    public async Task<TValue> AddOrUpdateAsync<TValue>(string key, Func<Task<TValue>> factory, DateTime timeout, bool isUtcTime = true, CancellationToken cancellationToken = default)
-    {
-        var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
-        return await AddOrUpdateAsync(key, factory, timespan, cancellationToken);
-    }
+	/// <inheritdoc />
+	public async Task<TValue> AddOrUpdateAsync<TValue>(string key, Func<Task<TValue>> factory, TimeSpan? timeout = null, CancellationToken cancellationToken = default)
+	{
+		key = RewriteKey(key);
+		var value = await factory();
+		return AddOrUpdate(key, value, timeout);
+	}
 
-    /// <inheritdoc />
-    public async Task<TValue> AddOrUpdateAsync<TValue>(Func<Task<CacheItem<TValue>>> factory, CancellationToken cancellationToken = default)
-    {
-        var item = await factory();
-        return AddOrUpdate(item);
-    }
+	/// <inheritdoc />
+	public async Task<TValue> AddOrUpdateAsync<TValue>(string key, Func<Task<TValue>> factory, DateTime timeout, bool isUtcTime = true, CancellationToken cancellationToken = default)
+	{
+		var timespan = timeout - (isUtcTime ? DateTime.UtcNow : DateTime.Now);
+		return await AddOrUpdateAsync(key, factory, timespan, cancellationToken);
+	}
 
-    private string RewriteKey(string key)
-    {
-        if (string.IsNullOrEmpty(_prefix))
-        {
-            return key;
-        }
+	/// <inheritdoc />
+	public async Task<TValue> AddOrUpdateAsync<TValue>(Func<Task<CacheItem<TValue>>> factory, CancellationToken cancellationToken = default)
+	{
+		var item = await factory();
+		return AddOrUpdate(item);
+	}
 
-        return key.StartsWith($"{_prefix}.Cache.", StringComparison.OrdinalIgnoreCase) ? key : $"{_prefix}.Cache.{key}";
-    }
+	private string RewriteKey(string key)
+	{
+		if (string.IsNullOrEmpty(_prefix))
+		{
+			return key;
+		}
 
-    private static CacheItem<TValue> GetCacheItem<TValue>(string key, TValue value, TimeSpan? timeout)
-    {
-        CacheItem<TValue> item;
-        if (timeout > TimeSpan.Zero)
-        {
-            item = new CacheItem<TValue>(key, value, CacheExpirationMode.Absolute, timeout.Value);
-        }
-        else
-        {
-            item = new CacheItem<TValue>(key, value);
-        }
-        return item;
-    }
+		return key.StartsWith($"{_prefix}.Cache.", StringComparison.OrdinalIgnoreCase) ? key : $"{_prefix}.Cache.{key}";
+	}
+
+	private static CacheItem<TValue> GetCacheItem<TValue>(string key, TValue value, TimeSpan? timeout)
+	{
+		CacheItem<TValue> item;
+		if (timeout > TimeSpan.Zero)
+		{
+			item = new CacheItem<TValue>(key, value, CacheExpirationMode.Absolute, timeout.Value);
+		}
+		else
+		{
+			item = new CacheItem<TValue>(key, value);
+		}
+		return item;
+	}
 }
